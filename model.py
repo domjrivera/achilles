68cb0b24e77e08d871dbeb1356b0a10cbd780229
Nicholas Strick <strickolas719@gmail.com>
2019-04-02 01:15:51 -0400

    Retrain model

--- model.py
+++ model.py
@@ -51,6 +51,3 @@ class AchillesModel:
         accr = model.evaluate(test_sequences_matrix, Y_test)
         model.save(os.path.dirname(__file__) + "/" + SAVE_MODEL_AS.replace("<language>", language), overwrite=MODEL_OVERWRITE)
         print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}\n'.format(accr[0], accr[1]))
-
-
-# AchillesModel.train("java")
