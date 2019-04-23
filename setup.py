d61fcd5350a3f58c5dbfefe178db5abaa973616a
Nicholas Strick <strickolas719@gmail.com>
2019-04-22 22:53:44 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='achilles',
+    version='1.2',
+    description='Static vulnerability detection using machine learning & magic!',
+    license="MIT",
+    long_description=long_description,
+    author='Nicholas Saccente',
+    author_email='nicholas.j.saccente@gmail.com',
+    url="https://github.com/strickolas/achilles",
+    packages=['achilles'],
+    install_requires=['pandas', 'numpy', 'keras', 'tensorflow', 'numpy', 'sklearn',
+                      'javalang'],
+)
